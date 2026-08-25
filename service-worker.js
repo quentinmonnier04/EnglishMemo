d38b36122bd48189aae1a60fcf730240f11c9d5e
quentinmonnier04 <quentin.monnier.04@gmail.com>
2026-08-25 11:59:45 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "english-memo-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./vocabulaire.json",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+
+    );
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(response => {
+
+                return response || fetch(event.request);
+
+            })
+
+    );
+
+});
